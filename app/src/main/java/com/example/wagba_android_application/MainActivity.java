90a0f26c9611d9e05b3cf35f0e7f9146b8bf0c61
sama <sama2ayman2@gmail.com>
2022-12-07 14:33:14 +0200

    Navigation

--- app/src/main/java/com/example/wagba_android_application/MainActivity.java
+++ app/src/main/java/com/example/wagba_android_application/MainActivity.java
@@ -1,9 +1,17 @@
 
 package com.example.wagba_android_application;
 
+
+import static androidx.navigation.ActivityKt.findNavController;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +19,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavHostFragment navHostFragment =
+                (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
+        NavController navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 }
