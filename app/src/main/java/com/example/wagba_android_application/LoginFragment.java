bc5be928c8299a45439bd3410112dfe219f2fdbf
sama <sama2ayman2@gmail.com>
2022-12-19 11:42:11 +0200

    google sign in

--- app/src/main/java/com/example/wagba_android_application/LoginFragment.java
+++ app/src/main/java/com/example/wagba_android_application/LoginFragment.java
@@ -1,16 +1,32 @@
 package com.example.wagba_android_application;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class LoginFragment extends Fragment {
 
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+    NavController nav;
+    Button loginBtn;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,13 +37,40 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_login, container, false);
-        Button loginBtn = view.findViewById(R.id.loginBtn);
+
+        loginBtn = view.findViewById(R.id.loginBtn);
+
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(getActivity(), gso);
+
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Navigation.findNavController(view).navigate(R.id.action_loginFragment_to_restaurantsFragment);
+                nav = Navigation.findNavController(view);
+                signIn();
             }
         });
         return view;
+    }
+    void signIn(){
+        Intent signInIntent = gsc.getSignInIntent();
+        startActivityForResult(signInIntent, 1000);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1000){
+
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                task.getResult(ApiException.class);
+                nav.navigate(R.id.action_loginFragment_to_restaurantsFragment);
+
+            } catch (ApiException e) {
+                Toast.makeText(getActivity().getApplicationContext(), "Something went wrong", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
